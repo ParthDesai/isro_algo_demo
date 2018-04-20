b020177447a745d448b90302ced06306b3514c7f
Parth Desai <desaiparth08@gmail.com>
2018-04-20 23:58:00 +0530

    Adding main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/parthdesai/teliaxProj/lib"
+)
+
+func main() {
+
+	numberOfLSPtr := flag.Int("numberOfLS", 2, "Number of launch station")
+	numberOfSatellitesToLaunchPtr := flag.Int("satToLaunch", 500, "Total number of satellite to launch")
+	perLSLaunchCapacityPtr := flag.Int("perLSLaunchCapacity", 4, "Per Launch station launch capacity")
+	flag.Parse()
+
+	if *numberOfLSPtr <= 0 || *numberOfSatellitesToLaunchPtr <= 0 || *perLSLaunchCapacityPtr <= 0 {
+		panic(fmt.Errorf("Parameters should be greater than zero"))
+	}
+
+	isro := lib.ISRO{}
+	isro.Init(*numberOfLSPtr, *perLSLaunchCapacityPtr, &lib.PrintReporter{})
+	isro.StartLaunch(*numberOfSatellitesToLaunchPtr)
+}
